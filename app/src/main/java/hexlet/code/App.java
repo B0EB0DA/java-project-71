fcac97bb15c9182afbd817164bac8cdbf25fd86a
B0EB0DA <GordienkoM@gmail.com>
2024-04-01 01:11:45 +0300

    Stage 4.0

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,13 +5,20 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.io.File;
+import java.nio.file.Path;
+
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 
 public class App {
 
-//    @CommandLine.Option(names = { "-h", "--help" }, usageHelp = true, description = "display a help message")
-//    private boolean helpRequested = false;
+    @Parameters(index = "0", description = "path to first file")
+    private Path filepath1;
+    @Parameters(index = "1", description = "path to second file")
+    private Path filepath2;
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
