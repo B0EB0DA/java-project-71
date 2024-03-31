6597913aa0ee78c5828f23e0741933b7e6e4400f
B0EB0DA <GordienkoM@gmail.com>
2024-04-01 00:32:43 +0300

    Stage 3.0

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,21 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+
 public class App {
 
+//    @CommandLine.Option(names = { "-h", "--help" }, usageHelp = true, description = "display a help message")
+//    private boolean helpRequested = false;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 
 }
